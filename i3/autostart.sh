5f29fde0f22227ca3709ee1adb16d9b10b09c6f5
boody <bodebi639@gmail.com>
2024-04-17 02:20:10 +0200

    Better sens settings

--- i3/autostart.sh
+++ i3/autostart.sh
@@ -12,7 +12,7 @@ dunst &
 xrandr --newmode "1920x1080_60.00"  172.80  1920 2040 2248 2576  1080 1081 1084 1118  -HSync +Vsync &
 xrandr --addmode VGA-1 "1920x1080_60.00" &
 xrandr --output VGA-1 --mode "1920x1080_60.00" &
-xinput --set-prop "MOSART Semi. Trust Wireless Mouse" "Coordinate Transformation Matrix" 0.5 0 0 0 0.5 0 0 0 1 &
+xinput set-prop "MOSART Semi. Trust Wireless Mouse" "libinput Accel Speed" -0.6 &
 # Language
 setxkbmap -layout us,ar &
 setxkbmap -option 'grp:alt_shift_toggle' &
