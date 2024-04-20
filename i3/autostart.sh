c34170451ed3a739c9cdda51c70988885907ef1f
boody <bodebi639@gmail.com>
2024-04-20 20:32:27 +0200

    fixed the dumb shit I broke

--- i3/autostart.sh
+++ i3/autostart.sh
@@ -1,6 +1,5 @@
 # Autostart applications
 # /usr/lib/x86_64-linux-gnu/polkit-mate/polkit-mate-authentication-agent-1 &
-~/scrnfix.sh &
 /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
 # nitrogen --restore; sleep 1; 
 feh --bg-fill ~/.config/backgrounds/gruvbox1.png & picom -b &
@@ -9,7 +8,6 @@ nm-applet &
 volumeicon &
 flameshot &
 dunst &
-sleep 2 &
 xrandr --newmode "1920x1080_60.00"  172.80  1920 2040 2248 2576  1080 1081 1084 1118  -HSync +Vsync &
 xrandr --addmode VGA-1 "1920x1080_60.00" &
 xrandr --output VGA-1 --mode "1920x1080_60.00" &
