a10b2f742947656231dc2595f544f6059eabd2b9
boody <bodebi639@gmail.com>
2024-04-20 19:49:57 +0200

    fix

--- i3/autostart.sh
+++ i3/autostart.sh
@@ -9,7 +9,6 @@ nm-applet &
 volumeicon &
 flameshot &
 dunst &
-xrandr -s 0 & 
 sleep 2 &
 xrandr --newmode "1920x1080_60.00"  172.80  1920 2040 2248 2576  1080 1081 1084 1118  -HSync +Vsync &
 xrandr --addmode VGA-1 "1920x1080_60.00" &
